67c26a0679bf7176e514e7734871da25c8c237d5
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-26 13:57:52 +1000

    fish jumping function

--- Fish.cs
+++ Fish.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class Fish : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] // This creates displays in the editor
+    private float jumpForce = 500f;
 
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
-    {
-        
-    }
+	{
+		if (Input.GetButtonDown("Fire1"))
+        {
+            // this is stopping th downward velocity so the jump van be performed again
+            rb.velocity = Vector2.zero;
+
+            // this is the jump action using the .up and multiplying it by the jumpForce
+            rb.AddForce(Vector2.up * jumpForce);
+        }
+	}
 }
