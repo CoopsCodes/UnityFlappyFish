58033c1b4c43b64299eca3497cdf6d9585bfed7d
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-26 14:49:35 +1000

    seaweed scrolling functionality

---
+++ Seaweed.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Seaweed : MonoBehaviour
+{
+    // OnCollisionEnter is used to indicate when two objects have collided(touched)
+    private void OnCollisionEnter2D(Collision2D collide)
+    {
+        // Indicating if the collide.collider intersects with the GetComponent object
+        {
+            if (collide.collider.GetComponent<Fish>() != null)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
---
+++ ViewMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 1.5f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.position += Time.deltaTime * speed * Vector3.left;
+    }
+}
