23b65127cb4a7acbfae565cdf2d9b0c505df7f7d
Cooper Viktor <im.cooperviktor@gmail.com>
2020-06-26 15:22:49 +1000

    adding random heights to the seaweeds

--- ViewMovement.cs
+++ ViewMovement.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class ViewMovement : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 1.5f;
+    private float speed = 2f;
+
+    private float posX = -15f;
+
+    [SerializeField]
+    private float randomOffset = 2f;
 
     // Update is called once per frame
     private void Update()
     {
         transform.position += Time.deltaTime * speed * Vector3.left;
+
+        if (transform.position.x <= posX)
+        {
+            float randomHeight = Random.Range(-randomOffset, randomOffset);
+            transform.position = new Vector3(15, randomHeight, 0);
+        }
     }
 }
